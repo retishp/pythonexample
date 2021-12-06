1ad3f798da55ee2905690230aa12574526ccecf1
retishp <retishp@users.noreply.github.com>
2021-12-05 22:58:58 -0500

    Added new route to trigger error inbox errors in NR

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,5 +7,14 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python!"
 
+@app.route('/list', methods=['GET'])
+def userlist():
+  return userlist()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+def userlist():
+  usrArray = ['tom', 'joe', 'alice']
+  for x in range(10):
+      print(usrArray[x])
